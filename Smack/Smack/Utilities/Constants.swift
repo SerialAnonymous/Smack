58409896a9861b990c2e2915554a897ffe116e8e
Nayan Jariwala <nayanjariwala@Nayans-iMac.local>
2018-03-30 00:43:37 +0530

    Fourth Commit

---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nayan Jariwala on 30/03/18.
+//  Copyright © 2018 Ghost. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
--- Smack/Smack/View/GradientView.swift
+++ Smack/Smack/View/GradientView.swift
@@ -7,99 +7,27 @@
 //
 
 import UIKit
-
 @IBDesignable
 class GradientView: UIView {
-    var colorArray : [(color1: UIColor, color2: UIColor)]=[]
-    var currentColorArrayIndex = -1
-    @IBInspectable var color1of1: UIColor = UIColor.blue  {
-        didSet{
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.3631127477, green: 0.4045833051, blue: 0.8775706887, alpha: 1) {
+        didSet {
             self.setNeedsLayout()
         }
     }
-    @IBInspectable var color1of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color2of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color2of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color3of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color3of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color4of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color4of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color5of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color5of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color6of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color6of2: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color7of1: UIColor = UIColor.blue  {
-        didSet{
-            self.setNeedsLayout()
-        }
-    }
-    @IBInspectable var color7of2: UIColor = UIColor.blue  {
-        didSet{
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
             self.setNeedsLayout()
         }
     }
     
     override func layoutSubviews() {
-        colorArray.append((color1: color1of1, color2: color1of2))
-        colorArray.append((color1: color2of1, color2: color2of2))
-        colorArray.append((color1: color3of1, color2: color3of2))
-        colorArray.append((color1: color4of1, color2: color4of2))
-        colorArray.append((color1: color5of1, color2: color5of2))
-        colorArray.append((color1: color6of1, color2: color6of2))
-        colorArray.append((color1: color7of1, color2: color7of2))
-        animatedBackground()
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-    func animatedBackground(){
-        currentColorArrayIndex = currentColorArrayIndex == (colorArray.count - 1) ? 0 : currentColorArrayIndex + 1
-        UIView.transition(with: self, duration: 10, options: [.transitionCrossDissolve], animations: {
-            self.backgroundColor = self.colorArray[self.currentColorArrayIndex].color1
-        }) { (completion) in
-            self.animatedBackground()
-        }
-    }
+
 }
