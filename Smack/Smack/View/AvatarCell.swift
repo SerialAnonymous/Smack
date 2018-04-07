63322c155d3b09dbe2aac1e15f47c08da398c4e2
Nayan Jariwala <nayanjariwala@Nayans-iMac.local>
2018-04-07 08:59:51 +0530

    done choose avatar

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,13 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Nayan Jariwala on 07/04/18.
+//  Copyright © 2018 Ghost. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+}
